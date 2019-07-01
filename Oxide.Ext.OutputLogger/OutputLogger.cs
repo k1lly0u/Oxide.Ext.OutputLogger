9302670813c180d040a0416ff01075f31de9c0f5
k1lly0u <k1lly0u@chaoscode.io>
2019-07-01 17:58:14 +0930

    Add files via upload

---
+++ Oxide.Ext.OutputLogger/OutputLogger.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.IO;
+using Oxide.Core;
+using UnityEngine;
+
+namespace Oxide.Ext.OutputLogger
+{
+    public class OutputLogger : Core.Logging.Logger
+    {
+        private string filePath;
+
+        public OutputLogger() : base(true)
+        {
+            string directory = Directory.GetParent(Application.dataPath).FullName;
+
+            string oldFilePath = Path.Combine(directory, "output_old.txt");
+
+            filePath = Path.Combine(directory, "output.txt");
+
+            if (File.Exists(oldFilePath))
+                File.Delete(oldFilePath);
+
+            if (File.Exists(filePath))            
+                File.Move(filePath, oldFilePath);            
+
+            Facepunch.Output.OnMessage += HandleOutputMessage;
+        }  
+
+        private void HandleOutputMessage(string message, string stackTrace, UnityEngine.LogType logType)
+        {
+            Core.Logging.LogType coreType = Core.Logging.LogType.Info;
+
+            switch (logType)
+            {
+                case LogType.Error:
+                    coreType = Core.Logging.LogType.Error;
+                    break;                
+                case LogType.Warning:
+                    coreType = Core.Logging.LogType.Warning;
+                    break;
+                case LogType.Exception:
+                    coreType = Core.Logging.LogType.Error;
+                    break;
+                case LogType.Assert:
+                case LogType.Log:
+                default:
+                    coreType = Core.Logging.LogType.Info;
+                    break;
+            }
+
+            this.Write(coreType, string.Concat(message, Environment.NewLine, stackTrace));
+        }
+
+        public override void HandleMessage(string message, string stackTrace, Oxide.Core.Logging.LogType logType)
+        {           
+            this.Write(logType, string.Concat(message, Environment.NewLine, stackTrace));
+        }
+                
+        public override void WriteException(string message, Exception ex)
+        {
+            string formatted = ExceptionHandler.FormatException(ex);
+            if (formatted != null)
+            {
+                this.Write(Core.Logging.LogType.Error, $"{message}{Environment.NewLine}{formatted}");
+                return;
+            }
+
+            Exception outerEx = ex;
+            while (ex.InnerException != null)
+            {
+                ex = ex.InnerException;
+            }
+
+            if (outerEx.GetType() != ex.GetType())
+            {
+                this.Write(Core.Logging.LogType.Error, "ExType: {0}", outerEx.GetType().Name);
+            }
+
+            this.Write(Core.Logging.LogType.Error, $"{message} ({ex.GetType().Name}: {ex.Message})\n{ex.StackTrace}");
+        }
+
+        public override void Write(Core.Logging.LogType type, string format, params object[] args)
+        {
+            this.Write(this.CreateLog(type, format, args));
+        }
+
+
+        protected Log CreateLog(Core.Logging.LogType type, string format, object[] args)
+        {
+            Log logMessage = new Log();
+
+            DateTime now = DateTime.Now;
+
+            logMessage.Message = string.Format("{0} [{1}] {2}", now.ToShortTimeString(), type, format);
+
+            return logMessage;
+        }
+
+        protected void Write(Log message)
+        {
+            using (StreamWriter streamWriter = new StreamWriter(filePath, true))
+            {
+                streamWriter.WriteLine(message.Message);
+            }
+        }
+
+        protected struct Log
+        {
+            public string Message;
+        }
+    }
+}
---
+++ Oxide.Ext.OutputLogger/OutputLoggerExtension.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Reflection;
+using Oxide.Core;
+using Oxide.Core.Extensions;
+
+namespace Oxide.Ext.OutputLogger
+{
+    public class OutputLoggerExtension : Extension
+    {
+        public OutputLoggerExtension(ExtensionManager manager) : base(manager)
+        {            
+        }
+
+        public override string Name => "Output Logger";
+
+        public override string Author => "k1lly0u";
+
+        public override VersionNumber Version => new VersionNumber(1, 0, 0);
+
+        public override void Load()
+        {
+            Interface.Oxide.RootLogger.AddLogger(new OutputLogger());
+        }
+
+        public override void OnModLoad()
+        {
+            AppDomain.CurrentDomain.UnhandledException += (sender, exception) =>
+            {
+                Interface.Oxide.LogException("An exception was thrown!", exception.ExceptionObject as Exception);
+            };
+        }
+
+        public override bool SupportsReloading => false;
+
+        internal static Assembly Assembly = Assembly.GetExecutingAssembly();
+
+        internal static AssemblyName AssemblyName = Assembly.GetName();
+
+        internal static VersionNumber AssemblyVersion = new VersionNumber(AssemblyName.Version.Major, AssemblyName.Version.Minor, AssemblyName.Version.Build);
+
+        internal static string AssemblyAuthors = ((AssemblyCompanyAttribute)Attribute.GetCustomAttribute(Assembly, typeof(AssemblyCompanyAttribute), false)).Company;
+    }
+}
